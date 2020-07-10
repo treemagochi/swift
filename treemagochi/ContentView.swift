380323ef9a236b7a5b4451d6814d287739b19fa2
vauxhall <zero.griffin@gmail.com>
2020-07-10 11:44:32 +0200

    Initial Commit

---
+++ treemagochi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  treemagochi
+//
+//  Created by vaux on 10.07.20.
+//  Copyright © 2020 Ecosia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
